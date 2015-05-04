063c8757cebe4d658614107596d1271a650c53b5
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-04 22:00:57 +0200

    Use `@sindresorhus/df`

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 'use strict';
 
-var execFile = require('child_process').execFile;
+var df = require('@sindresorhus/df');
 
 module.exports = function (file, cb) {
-	execFile('df', ['-kP', file], function (err, stdout) {
+	df.file(file, function (err, data) {
 		if (err) {
 			cb(err);
 			return;
 		}
 
-		stdout = stdout.trim().split('\n').slice(1).map(function (el) {
-			var cl = el.split(/\s+(?=[\d\/])/);
-
-			return {
-				fs: cl[0],
-				size: parseInt(cl[1], 10) * 1024,
-				used: parseInt(cl[2], 10) * 1024,
-				available: parseInt(cl[3], 10) * 1024,
-				percent: parseInt(cl[4], 10) / 100,
-				mount: cl[5]
-			};
+		cb(null, {
+			fs: data.filesystem,
+			size: data.size,
+			used: data.used,
+			available: data.available,
+			percent: data.capacity,
+			mount: data.mountpoint
 		});
-
-		cb(null, stdout[0]);
 	});
 };
